927033ee75a6598b058cb97bf75f08d384da40c0
Björn Tirsén <bjorntirsen@gmail.com>
2021-11-03 14:17:53 +0100

    :sparkles: Basic types and objects, enum

--- app.ts
+++ app.ts
@@ -1,1 +1,37 @@
-console.log('hello');
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'Björn',
+//   age: 39,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author'],
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
+  name: 'Björn',
+  age: 39,
+  hobbies: ['Sports', 'Cooking'],
+  role: Role.ADMIN,
+};
+
+// person.role.push('admin');
+// person.role[1] = 10;
+
+console.log(person.name);
