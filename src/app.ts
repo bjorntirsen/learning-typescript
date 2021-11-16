58cc9bcae093730e0ea4498cf6eb2cdda1fb0936
Björn Tirsén <bjorntirsen@gmail.com>
2021-11-16 10:16:43 +0100

    :sparkles: Discriminated Unions

--- src/app.ts
+++ src/app.ts
@@ -47,4 +47,31 @@ function printEmployeeInfo(emp: UnknownEmployee) {
 
 // Can also use if (emp instaceof Admin)
 
-printEmployeeInfo(e1);
+// printEmployeeInfo(e1);
+
+// Discriminated Unions 
+interface Bird {
+  type: 'bird';
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: 'horse';
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case 'bird':
+      speed = animal.flyingSpeed;
+      break;
+    case 'horse':
+      speed = animal.runningSpeed;
+  }
+  console.log('Moving at speed: ' + speed);
+}
+
+moveAnimal({ type: 'bird', flyingSpeed: 10 });
